1e4f0449f6c1477037b68020b91cb308ac65f238
softmarshmallow <woojoo@softmarshmallow.com>
2020-09-24 11:23:35 +0900

    🔥 initial commit for engine (python)

---
+++ engine/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press ⌘⏎ to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
